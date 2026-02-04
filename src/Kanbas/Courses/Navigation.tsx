9ec0cdcafb2323e7f7b998769293330eebc6fbcd
huijuhnkim <136677687+huijuhnkim@users.noreply.github.com>
2026-02-04 11:29:40 +0900

    Implement Bootstrap in Navigation.tsx

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,16 +1,52 @@
 import { Link } from "react-router-dom";
 export default function CoursesNavigation() {
   return (
-    <div id="wd-courses-navigation">
-      <Link id="wd-course-home-link"    to="/Kanbas/Courses/1234/Home">Home</Link><br/>
-      <Link id="wd-course-modules-link" to="/Kanbas/Courses/1234/Modules">Modules</Link><br/>
-      <Link id="wd-course-piazza-link"  to="/Kanbas/Courses/1234/Piazza">Piazza</Link><br/>
-      <Link id="wd-course-zoom-link"    to="/Kanbas/Courses/1234/Zoom">Zoom</Link><br/>
-      <Link id="wd-course-quizzes-link" to="/Kanbas/Courses/1234/Assignments">
-          Assignments</Link><br/>
-      <Link id="wd-course-assignments-link" to="/Kanbas/Courses/1234/Quizzes">Quizzes
-        </Link><br/>
-      <Link id="wd-course-grades-link"  to="/Kanbas/Courses/1234/Grades">Grades</Link><br/>
-      <Link id="wd-course-people-link"  to="/Kanbas/Courses/1234/People">People</Link><br/>
+    <div id="wd-courses-navigation" className="wd list-group fs-6 rounded-0">
+      <Link className="course list-group-item active border border-0"
+      id="wd-course-home-link"    to="/Kanbas/Courses/1234/Home">
+        Home
+      </Link>
+      
+      <Link id="wd-course-modules-link" 
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Modules">
+        Modules
+      </Link>
+
+      <Link id="wd-course-piazza-link"  
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Piazza">
+        Piazza
+      </Link>
+
+      <Link id="wd-course-zoom-link"    
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Zoom">
+        Zoom
+      </Link>
+
+      <Link id="wd-course-quizzes-link"
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Assignments">
+        Assignments
+      </Link>
+
+      <Link id="wd-course-assignments-link"
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Quizzes">
+        Quizzes
+      </Link>
+
+      <Link id="wd-course-grades-link"
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/Grades">
+        Grades
+      </Link>
+
+      <Link id="wd-course-people-link"
+            className="course list-group-item border border-0 text-danger"
+            to="/Kanbas/Courses/1234/People">
+        People
+      </Link>
     </div>
 );}
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,14 +1,50 @@
 import { Link } from "react-router-dom";
+import { BsPersonCircle, BsBook, BsCalendar3, BsInbox, BsSpeedometer2 } from "react-icons/bs";
+import { IoFlaskOutline } from "react-icons/io5";
+
 
 export default function KanbasNavigation() {
   return (
-    <div id="wd-kanbas-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link">Northeastern</a><br/>
-      <Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kanbas/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+    <div id="wd-kanbas-navigation" 
+      className="list-group bg-black rounded-0 position-fixed bottom-0 top-0 z-2 d-none d-md-block" 
+      style={{width: "120px"}}>
+      <a className="list-group-item bg-black text-white text-center border-0" 
+      href="https://www.northeastern.edu/" id="wd-neu-link">Northeastern</a>
+      
+      <Link className="list-group-item bg-white text-danger text-center border-0" 
+      to="/Kanbas/Account" id="wd-account-link">
+        <BsPersonCircle className="fs-2"/> <br/>
+        Account 
+      </Link>
+      
+      <Link className="list-group-item bg-black text-white text-center border-0" 
+      to="/Kanbas/Dashboard" id="wd-dashboard-link">
+        <BsSpeedometer2 className="fs-3"/> <br/>
+        Dashboard
+      </Link>
+      
+      <Link className="list-group-item bg-black text-white text-center border-0" 
+      to="/Kanbas/Dashboard" id="wd-course-link">
+        <BsBook className="fs-3"/> <br/>
+        Courses
+      </Link>
+      
+      <Link className="list-group-item bg-black text-white text-center border-0" 
+      to="/Kanbas/Calendar" id="wd-calendar-link">
+        <BsCalendar3 className="fs-4"/> <br/>
+        Calendar
+      </Link>
+      
+      <Link className="list-group-item bg-black text-white text-center border-0" 
+      to="/Kanbas/Inbox" id="wd-inbox-link">
+        <BsInbox className="fs-3"/> <br/>
+        Inbox
+      </Link>
+      
+      <Link className="list-group-item bg-black text-white text-center border-0" 
+      to="/Labs" id="wd-labs-link">
+        <IoFlaskOutline className="fs-3"/> <br/>
+        Labs
+      </Link>
     </div>
 );}
